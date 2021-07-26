aeb8ff466e307b07a57bcb244a8364cccf1abad2
nhrnhr0 <nhrnhr0@gmail.com>
2021-07-26 11:56:50 +0300

    t

---
+++ setup/open_chromium_on_startup.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('ls -l')
+
+os.system('chromium -kiosk 127.0.0.1:8010 &')
--- setup/update.sh
+++ setup/update.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 WEBSITE_DIR="/home/pi/Desktop/rasberryWebsite3"
 SUPERVISOR_CONF_FILE="local_webserver.conf"
+PENV="$WEBSITE_DIR/env/bin/python3"
+PIPENV="$WEBSITE_DIR/env/bin/pip"
+echo '============ pull from git:'
 cd $WEBSITE_DIR
 sudo git pull
+$PIPENV install -r $WEBSITE_DIR/requirements.txt
+$PENV $WEBSITE_DIR/website/manage.py migrate
 sudo supervisorctl update
 sudo supervisorctl restart local_webserver
 sudo supervisorctl status
