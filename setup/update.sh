223d454794c6d8cc065fcef48060716ce10d8d4e
nhrnhr0 <nhrnhr0@gmail.com>
2021-07-28 05:21:21 +0300

    t

--- setup/update.sh
+++ setup/update.sh
@@ -6,13 +6,11 @@ PIPENV="$WEBSITE_DIR/env/bin/pip"
 echo '============ pull from git:'
 cd $WEBSITE_DIR
 sudo git pull
-sudo su
 echo '============ install requirements.txt:'
 $PIPENV install -r $WEBSITE_DIR/requirements.txt
 echo '============ migrate:'
 $PENV $WEBSITE_DIR/website/manage.py migrate
 echo '============ update supervisor:'
-su pi
 sudo supervisorctl update
 sudo supervisorctl restart local_webserver
 sudo supervisorctl status
